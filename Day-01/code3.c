ad6449a58c5f035c4b17428806af072a2c57fd80
Vinay Varshney <vinay.varshney2025@glbajajgroup.org>
2026-06-07 14:56:04 +0530

    Day 1: Completed all 4 codes

---
+++ Day-01/code3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int n,i;
+    unsigned long long fact = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    // Shows  error if the user enters  a negative integer
+    if (n<0)
+    printf("Error! Factorial of a negative no. doesn't exist.");
+    else{
+        for(i=1; i<=n; ++i){
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
+    return 0;
+}
